4f727683e3c9d3405c98b1759156d926284ce2e5
Matheus <mcost16@lsu.edu>
2024-07-24 12:00:20 -0300

    refactor: https check for production environment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}` || 'http://localhost:8000'
+    baseURL: `http${import.meta.env.VITE_IS_HTTPS ? "s" : ""}://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}` || 'http://localhost:8000'
 })
 
 export default api;
