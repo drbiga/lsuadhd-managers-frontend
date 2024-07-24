d5886a79843ae77935b221a7d2eb2cc8bfaa3afc
Matheus <mcost16@lsu.edu>
2024-07-24 10:47:46 -0300

    add: hardcoded backend url and port within api definition

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}` || 'http://localhost:8000'
+    // baseURL: `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}` || 'http://localhost:8000'
+    baseURL: 'http://localhost:8000'
 })
 
 export default api;
