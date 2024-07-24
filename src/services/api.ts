789f10213cf6f86b6681c20a5b932262632cf71f
Matheus <mcost16@lsu.edu>
2024-07-24 10:38:31 -0300

    add: default backend endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}`
+    baseURL: `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}` || 'http://localhost:8000'
 })
 
 export default api;
