1a2722392caaf51dddeac60fc3440ea98edb26d0
Matheus <mcost16@lsu.edu>
2024-08-09 16:10:11 -0500

    add: create and get all managers using management context

--- src/pages/Management.tsx
+++ src/pages/Management.tsx
@@ -1,13 +1,71 @@
+import { useCallback, useEffect, useState } from "react";
 import { PageContainer, PageMainContent, PageTitle } from "../components/Page"
 import Sidebar from "../components/Sidebar"
+import managementService, { Manager } from "@/services/managementService";
+import { toast } from "react-toastify";
+import { FieldValues, useForm } from "react-hook-form";
+import { useAuth } from "@/hooks/auth";
 
 export default function Management() {
-    return (
-        <PageContainer>
-            <Sidebar />
-            <PageMainContent>
-                <PageTitle>Management</PageTitle>
-            </PageMainContent>
-        </PageContainer>
-    )
+  const [managers, setManagers] = useState<Manager[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const managersResponse = await managementService.getManagers();
+      setManagers(managersResponse);
+    })()
+  }, []);
+
+  const {
+    reset,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const { authState } = useAuth();
+
+  const onSubmit = useCallback(async (data: FieldValues) => {
+    try {
+      const newManager = await managementService.createManager(data.name);
+      setManagers([...managers, newManager]);
+    } catch (error) {
+      toast.error('Unknown error. Please contact someone');
+      reset();
+    }
+  }, [authState]);
+
+  return (
+    <PageContainer>
+      <Sidebar />
+      <PageMainContent>
+        <PageTitle>Management</PageTitle>
+
+        <div>
+          <h2 className="text-slate-400 dark:text-slate-600 opacity-70 text-2xl mb-8">Existing managers</h2>
+          <ul>
+            {managers.map(m => (
+              <li className="mb-4" key={m.name}>
+                {m.name}
+              </li>
+            ))}
+          </ul>
+          <div className="max-w-80 flex flex-col">
+            <h2 className="text-slate-400 dark:text-slate-600 opacity-70 text-2xl mb-8">Create new manager</h2>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+              <input
+                type="text"
+                className="bg-accent p-2 rounded-lg"
+                placeholder="Name"
+                {...register("name", { required: true })}
+              />
+              <div>
+                <button type="submit" className="float-right bg-accent p-2 rounded-lg hover:bg-accent-foreground hover:text-accent transition-all duration-100">Create</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </PageMainContent>
+    </PageContainer >
+  )
 }
---
+++ src/services/managementService.ts
@@ -0,0 +1,33 @@
+import api from "./api";
+import iamService from "./iam";
+
+export type Manager = {
+    name: string;
+}
+
+class ManagementService {
+    public async createManager(managerName: string): Promise<Manager> {
+        const response = await api.post('/management/manager', {
+        }, {
+            params: {
+                manager_name: managerName,
+                name_manager_requesting_operation: iamService.getCurrentSession().user.username
+            }
+        });
+        return response.data;
+    }
+
+    public async getManagers(): Promise<Manager[]> {
+        const response = await api.get('/management/manager', {
+            params: {
+                name_manager_requesting_operation: iamService.getCurrentSession().user.username
+            }
+        });
+        return response.data;
+    }
+}
+
+
+const managementService = new ManagementService();
+
+export default managementService;
