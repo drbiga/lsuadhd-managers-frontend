6ef7692f8f24b77bc1b7c9a87aef28895230eb6f
Matheus <mcost16@lsu.edu>
2024-10-14 09:57:18 -0500

    Updates

--- src/services/api.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: `http${import.meta.env.VITE_IS_HTTPS === 'yes' ? "s" : ""}://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}` || 'http://localhost:8000'
-})
-export default api;
