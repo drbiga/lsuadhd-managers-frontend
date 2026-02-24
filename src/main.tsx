f2d4a64cb75105d5d33656c4151d66d271b41e81
drbiga <mcost16@lsu.edu>
2026-02-24 16:00:33 -0600

    add: session progress provider to root component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './hooks/auth.tsx'
+import { SessionProgressProvider } from './features/students/hooks/useSessionProgress.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <AuthProvider>
+  <AuthProvider>
+    <SessionProgressProvider>
       <App />
-    </AuthProvider>
+    </SessionProgressProvider>
+  </AuthProvider>
 )
