4c3c92bd941fc99d775b4932375481dad7c25b4c
drbiga <mcost16@lsu.edu>
2026-02-24 16:00:07 -0600

    refactor: made the use session progress a context instead of pure hook to preserve state across components

---
+++ src/features/students/hooks/useSessionProgress.tsx
@@ -0,0 +1,160 @@
+import { createContext, Dispatch, PropsWithChildren, SetStateAction, useCallback, useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import studentService, { Student, StudentWithSessionData } from "../services/studentService";
+import { useAuth } from "@/hooks/auth";
+
+export type SessionProgressState = {
+  student: StudentWithSessionData | null,
+  setStudent: Dispatch<SetStateAction<StudentWithSessionData | null>>;
+  allStudents: Student[],
+  handleStudentChange: (selectedStudentName: string | null) => Promise<void>,
+  descriptions: any[],
+  loading: boolean,
+  studentsLoading: boolean,
+  studentDataLoading: boolean,
+  selectedSession: number | null,
+  fetchImageDescriptions: (studentName: string, sessionSeqnum: number, loadMore?: boolean) => Promise<void>
+  handleDeleteSession: (
+    sessionNum: number,
+  ) => Promise<void>
+}
+
+const SessionProgressContext = createContext<SessionProgressState>({} as SessionProgressState);
+
+export function SessionProgressProvider({ children }: PropsWithChildren) {
+  const { authState } = useAuth();
+  const [student, setStudent] = useState<StudentWithSessionData | null>(null);
+  const [allStudents, setAllStudents] = useState<Student[]>([]);
+  const [descriptions, setImageDescriptions] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [studentsLoading, setStudentsLoading] = useState(true);
+  const [studentDataLoading, setStudentDataLoading] = useState(false);
+  const [selectedSession, setSelectedSession] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (!authState.isLoggedIn) {
+      setStudentsLoading(false);
+      return;
+    }
+
+    (async () => {
+      setStudentsLoading(true);
+      try {
+        const response = await studentService.getAllStudents();
+        setAllStudents(response);
+      } catch (err) {
+        console.error('Error fetching students:', err);
+        toast.error("Failed to fetch student data");
+      } finally {
+        setStudentsLoading(false);
+      }
+    })();
+  }, [authState.isLoggedIn]);
+
+  const handleStudentChange = useCallback(async (selectedStudentName: string | null) => {
+    if (!selectedStudentName) {
+      setStudent(null);
+      return;
+    }
+
+    setStudentDataLoading(true);
+    try {
+      const studentData = await studentService.getStudentWithSessionData(selectedStudentName);
+      setStudent(studentData);
+    } catch (err) {
+      console.error('Error fetching student data:', err);
+      toast.error("Failed to fetch student session data");
+      setStudent(null);
+    } finally {
+      setStudentDataLoading(false);
+    }
+  }, []);
+
+  const fetchImageDescriptions = useCallback(async (studentName: string, sessionSeqnum: number, loadMore = false) => {
+    setLoading(true);
+    if (!loadMore) {
+      setSelectedSession(sessionSeqnum);
+      setImageDescriptions([]);
+    }
+
+    try {
+      const offset = loadMore ? descriptions.length : 0;
+      const data = await studentService.getImageDescriptions(studentName, sessionSeqnum, offset, 10);
+      setImageDescriptions(prev => loadMore ? [...prev, ...data] : data);
+    } catch (error) {
+      console.error('Error:', error);
+      if (!loadMore) setImageDescriptions([]);
+    }
+    setLoading(false);
+  }, [descriptions]);
+
+  const handleDeleteSession = useCallback(
+    async (
+      sessionNum: number,
+    ) => {
+      if (student === null) {
+        toast.error('You need to choose a student before deleting. This should not happen?!')
+        return;
+      }
+      try {
+        await studentService.deleteSession(student.name, sessionNum);
+        console.log('==============================================')
+        console.log('[ handleDeleteSession ] Updating student object')
+        setStudent(previousStudent => {
+          if (previousStudent === null) {
+            toast.error('You need to choose a student before deleting. This should not happen?!')
+            return previousStudent;
+          }
+          return {
+            ...previousStudent,
+            sessions: previousStudent?.sessions.filter(s => s.seqnum !== sessionNum)
+          }
+        });
+        console.log('==============================================')
+        toast.success(`Deleted session ${sessionNum} for student ${student.name}`);
+      } catch (error) {
+        toast.error(error instanceof Error ? error.message : "Failed to lock user");
+      }
+    }, [student]);
+
+  // return {
+  //   student,
+  //   allStudents,
+  //   handleStudentChange,
+  //   descriptions,
+  //   loading,
+  //   studentsLoading,
+  //   studentDataLoading,
+  //   selectedSession,
+  //   fetchImageDescriptions,
+  //   handleDeleteSession
+  // };
+  return (
+    <SessionProgressContext.Provider value={{
+      student,
+      setStudent,
+      allStudents,
+      handleStudentChange,
+      descriptions,
+      loading,
+      studentsLoading,
+      studentDataLoading,
+      selectedSession,
+      fetchImageDescriptions,
+      handleDeleteSession
+    }}>
+      {children}
+    </SessionProgressContext.Provider>
+  )
+}
+
+
+export function useSessionProgress() {
+  const context = useContext(SessionProgressContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  return context;
+}
